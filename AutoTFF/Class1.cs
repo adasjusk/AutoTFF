2c2ae457bcf62938cd7988bec2ed22e6ee50cd56
Adas <adasjusk@oranges.lt>
2026-06-27 18:16:35 +0300

    2.0

--- AutoTFF/Class1.cs
+++ AutoTFF/Class1.cs
@@ -2,53 +2,21 @@
 using LabApi.Events;
 using LabApi.Events.Arguments.ServerEvents;
 using LabApi.Events.Handlers;
-using LabApi.Features;
 using LabApi.Features.Console;
 using LabApi.Features.Wrappers;
 using LabApi.Loader.Features.Plugins;
-
 namespace AutoTFF
 {
     public sealed class Class1 : Plugin
     {
         public static Class1 Instance { get; private set; }
-
         public override string Name => "AutoTFF";
-
         public override string Author => "adasjusk";
-
-        public override Version Version { get; } = new Version(1, 0, 0);
-
+        public override Version Version { get; } = new Version(2, 0, 0);
         public override string Description => "Enables friendly fire on round end and disables it on round start.";
-
-        public override Version RequiredApiVersion
-        {
-            get
-            {
-                try
-                {
-                    object compiledVersion = LabApiProperties.CompiledVersion;
-                    if (compiledVersion is Version result)
-                    {
-                        return result;
-                    }
-                    string text = compiledVersion?.ToString();
-                    if (!string.IsNullOrEmpty(text) && Version.TryParse(text, out var result2))
-                    {
-                        return result2;
-                    }
-                }
-                catch
-                {
-                }
-                return new Version(1, 0, 0);
-            }
-        }
-
+        public override Version RequiredApiVersion { get; } = new Version(1, 1, 7);
         public override void Enable()
         {
-            //IL_001e: Unknown result type (might be due to invalid IL or missing references)
-            //IL_0028: Expected O, but got Unknown
             Instance = this;
             ServerEvents.RoundEnded += OnRoundEnded;
             ServerEvents.RoundStarted += new LabEventHandler(OnRoundStarted);
@@ -62,16 +30,12 @@ namespace AutoTFF
                 Logger.Error(ex);
             }
         }
-
         public override void Disable()
         {
-            //IL_0018: Unknown result type (might be due to invalid IL or missing references)
-            //IL_0022: Expected O, but got Unknown
             ServerEvents.RoundEnded -= OnRoundEnded;
             ServerEvents.RoundStarted -= new LabEventHandler(OnRoundStarted);
             Instance = null;
         }
-
         private void OnRoundEnded(RoundEndedEventArgs ev)
         {
             try
@@ -84,7 +48,6 @@ namespace AutoTFF
                 Logger.Error(ex);
             }
         }
-
         private void OnRoundStarted()
         {
             try
@@ -96,6 +59,4 @@ namespace AutoTFF
             {
                 Logger.Error(ex);
             }
-        }
-    }
-}
+}   }   }
